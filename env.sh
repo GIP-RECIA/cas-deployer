62968519dea2971adac17c3c4260ffa9aee402d9
cailbourdin <nathan.cailbourdin@recia.fr>
2024-10-21 09:58:29 +0200

    conf: fix -Xmx memory

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ export CATALINA_TMPDIR=$CATALINA_BASE/temp
 export LOG_PATH=
 export LOG_DIR=
 
-export JAVA_OPTS="$JAVA_OPTS -server -Xms1G -Xmx4m -XX:+UseG1GC -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
+export JAVA_OPTS="$JAVA_OPTS -server -Xms1G -Xmx4G -XX:+UseG1GC -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
 export JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true -Dnetworkaddress.cache.ttl=3600"
 export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true -Dcom.sun.management.jmxremote -Dhttps.protocols=TLSv1.2"
 export JAVA_OPTS="$JAVA_OPTS -Dhttp.agent=Java-ENT"
